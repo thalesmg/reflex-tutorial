75eb693bc103c8773ff0b27db5680642f3be3039
Thales Macedo Garitezi <thalesmg@gmail.com>
2019-10-02 15:45:38 -0300

    Ex04 solution

--- code/exercises/src/Ex04/Exercise.hs
+++ code/exercises/src/Ex04/Exercise.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 module Ex04.Exercise where
 
 import qualified Data.Map as Map
 
 import Reflex
+import Control.Monad (void)
 
 #ifndef ghcjs_HOST_OS
 import Util.Run
@@ -18,14 +21,37 @@ ex04 ::
   Outputs t
 ex04 (Inputs bMoney bCarrot bCelery bCucumber bSelected eBuy eRefund) =
   let
+    tryBuy (money, pstock) = do
+      void $ checkMoney money pstock
+      checkStock pstock
+    checkMoney money pstock =
+      if money >= pCost (sProduct pstock)
+      then Right pstock
+      else Left NotEnoughMoney
+    checkStock pstock =
+      if sQuantity pstock > 0
+      then Right $ sProduct pstock
+      else Left ItemOutOfStock
+    (eError, eBought)
+      = fanEither
+      . fmap tryBuy
+      . attachWithMaybe
+          (\maybeProduct money -> (money,) <$> maybeProduct)
+          bmaybeProduct
+      $ bMoney <@ eBuy
+    bmaybeProduct = do
+      productName <- bSelected
+      case productName of
+        "Carrot" -> Just <$> bCarrot
+        "Celery" -> Just <$> bCelery
+        "Cucumber" -> Just <$> bCucumber
+        _ -> pure Nothing
     eVend =
-      never
+      ffor eBought pName
     eSpend =
-      never
+      ffor eBought pCost
     eChange =
-      never
-    eError =
-      never
+      bMoney <@ eRefund
   in
     Outputs eVend eSpend eChange eError
 
