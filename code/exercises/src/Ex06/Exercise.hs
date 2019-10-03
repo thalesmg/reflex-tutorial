c55703235121f4a5687ea067534322a642d5e0e8
Thales Macedo Garitezi <thalesmg@gmail.com>
2019-10-03 09:23:23 -0300

    Ex06 solution

--- code/exercises/src/Ex06/Exercise.hs
+++ code/exercises/src/Ex06/Exercise.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE CPP #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE CPP #-}
 module Ex06.Exercise where
 
+import Control.Monad (void)
 import Control.Monad.Fix (MonadFix)
 
 import Data.Text (Text)
@@ -16,6 +18,31 @@ import Util.Run
 import Ex06.Common
 import Ex06.Run
 
+dChange :: MonadHold t m
+        => Reflex t
+        => Event t ()
+        -> Event t ()
+        -> Dynamic t Money
+        -> m (Dynamic t Money)
+dChange eAny eRefund dMoney =
+  holdDyn 0 $
+    leftmost [ current dMoney <@ eRefund
+             , 0 <$ eAny
+             ]
+
+dVend :: MonadHold t m
+      => Reflex t
+      => Event t Text
+      -> Event t Error
+      -> Event t ()
+      -> m (Dynamic t Text)
+dVend eVend eError eAny =
+  holdDyn "" $
+    leftmost [ errorText <$> eError
+             , eVend
+             , "" <$ eAny
+             ]
+
 ex06 ::
   ( Reflex t
   , MonadFix m
@@ -23,22 +50,50 @@ ex06 ::
   ) =>
   Inputs t ->
   m (Outputs t)
-ex06 (Inputs dMoney dCarrot dCelery dCucumber dSelected eBuy eRefund) =
+ex06 (Inputs dMoney dCarrot dCelery dCucumber dSelected eBuy eRefund) = do
   let
+    bMoney = current dMoney
+    tryBuy (money, pstock) = do
+      void $ checkMoney money pstock
+      checkStock pstock
+    checkMoney money pstock =
+      if money >= pCost (sProduct pstock)
+      then Right pstock
+      else Left NotEnoughMoney
+    checkStock pstock =
+      if sQuantity pstock > 0
+      then Right $ sProduct pstock
+      else Left ItemOutOfStock
+    (eError, eBought)
+      = fanEither
+      . fmap tryBuy
+      . attachWithMaybe
+          (\maybeProduct money -> (money,) <$> maybeProduct)
+          bmaybeStock
+      $ bMoney <@ eBuy
+    dmaybeStock = do
+      productName <- dSelected
+      case productName of
+        "Carrot" -> Just <$> dCarrot
+        "Celery" -> Just <$> dCelery
+        "Cucumber" -> Just <$> dCucumber
+        _ -> pure Nothing
+    bmaybeStock = current dmaybeStock
     eVend =
-      never
+      ffor eBought pName
     eSpend =
-      never
+      ffor eBought pCost
     eChange =
-      never
-    eError =
-      never
-    dChange =
-      pure 0
-    dVend =
-      pure ""
-  in do
-    pure $ Outputs eVend eSpend eChange eError dChange dVend
+      bMoney <@ eRefund
+    eAny =
+      leftmost [ () <$ updated dCarrot
+               , () <$ updated dCelery
+               , () <$ updated dCucumber
+               , eBuy
+               ]
+  dChange' <- dChange eAny eRefund dMoney
+  dVend' <- dVend eVend eError eAny
+  pure (Outputs eVend eSpend eChange eError dChange' dVend')
 
 #ifndef ghcjs_HOST_OS
 go ::
